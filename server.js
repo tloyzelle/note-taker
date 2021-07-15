9d4fe30e2699b02e5a0108971b5f83676e8c2329
Taegan Loyzelle <tloyzelle@gmail.com>
2021-07-15 15:36:35 -0400

    debuggin links within server.js

--- server.js
+++ server.js
@@ -9,23 +9,23 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
     });
 });
 
-app.post("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
         if (err) throw err;
         const db = JSON.parse(data);
         const newDB = [];
@@ -43,16 +43,16 @@ app.post("/api/notes", (req, res) => {
             newDB.push(newNote);
         }
 
-        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(newDB, null, 2), (err) => {
             if (err) throw err;
             res.json(req.body);
         });
     });
 });
 
-app.delete("/api/notes/:id", (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
         if (err) throw err;
         const db = JSON.parse(data);
         const newDB = [];
@@ -71,7 +71,7 @@ app.delete("/api/notes/:id", (req, res) => {
             }
         }
 
-        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(newDB, null, 2), (err) => {
             if (err) throw err;
             res.json(req.body);
         });
